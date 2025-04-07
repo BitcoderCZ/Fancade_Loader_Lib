5826a77b9fe27fc6025f3c0fc46f21735bc48ce8
Bitcoder <82174496+BitcoderCZ@users.noreply.github.com>
2025-04-07 11:22:17 +0200

    Add some stuff

--- src/FancadeLoaderLib.Runtime/AST.cs
+++ src/FancadeLoaderLib.Runtime/AST.cs
@@ -6,11 +6,13 @@ namespace FancadeLoaderLib.Runtime;
 
 public sealed class AST
 {
+    private List<object> _entryPoints;
+
     private AST()
     {
     }
 
-    public static AST Create(PrefabList prefabs, ushort mainId)
+    public static AST Create(PrefabList prefabs, ushort mainId, IRuntimeGame game)
     {
 
     }
