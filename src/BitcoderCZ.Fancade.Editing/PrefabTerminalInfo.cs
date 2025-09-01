5bf262cec5fc120da98ede2f1fee9fb44dc308c1
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2025-09-01 20:33:53 +0200

    Create terminals from connections in PrefabTerminalInfo .Create

--- src/BitcoderCZ.Fancade.Editing/PrefabTerminalInfo.cs
+++ src/BitcoderCZ.Fancade.Editing/PrefabTerminalInfo.cs
@@ -45,11 +45,11 @@ public readonly struct PrefabTerminalInfo
     public readonly IEnumerable<TerminalInfo> OutputTerminals => Terminals.Where(info => !info.IsInput);
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="PrefabTerminalInfo"/> struct from a <see cref="Prefab"/>.
+    /// Initializes a new instance of the <see cref="PrefabTerminalInfo"/> struct from a <see cref="Prefab"/>, creating terminals only from <paramref name="prefab"/>'s setting, ignoring connections.
     /// </summary>
     /// <param name="prefab">The <see cref="Prefab"/> to create the <see cref="PrefabTerminalInfo"/> from.</param>
     /// <returns>The <see cref="PrefabTerminalInfo"/> created from <paramref name="prefab"/>.</returns>
-    public static PrefabTerminalInfo Create(Prefab prefab)
+    public static PrefabTerminalInfo CreateFromSettingsOnly(Prefab prefab)
     {
         ImmutableArray<TerminalInfo>.Builder infoBuilder = ImmutableArray.CreateBuilder<TerminalInfo>(2);
 
@@ -60,6 +60,7 @@ public readonly struct PrefabTerminalInfo
                 continue;
             }
 
+            // TODO: only use index 0?
             foreach (var item in settings)
             {
                 if (item is not { } setting)
@@ -100,21 +101,158 @@ public readonly struct PrefabTerminalInfo
     }
 
     /// <summary>
+    /// Initializes a new instance of the <see cref="PrefabTerminalInfo"/> struct from a <see cref="Prefab"/>.
+    /// </summary>
+    /// <param name="prefab">The <see cref="Prefab"/> to create the <see cref="PrefabTerminalInfo"/> from.</param>
+    /// <param name="prefabList">A <see cref="PrefabList"/> used to resolve a terminal's type.</param>
+    /// <returns>The <see cref="PrefabTerminalInfo"/> created from <paramref name="prefab"/>.</returns>
+    public static PrefabTerminalInfo Create(Prefab prefab, PrefabList prefabList)
+        => Create(prefab, id => prefabList.TryGetSegments(id, out var segment) && prefabList.TryGetPrefab(segment.PrefabId, out var prefab) ? prefab : null);
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PrefabTerminalInfo"/> struct from a <see cref="Prefab"/>.
+    /// </summary>
+    /// <param name="prefab">The <see cref="Prefab"/> to create the <see cref="PrefabTerminalInfo"/> from.</param>
+    /// <param name="getPrefab">Gets a <see cref="Prefab"/>, from a segment id.</param>
+    /// <returns>The <see cref="PrefabTerminalInfo"/> created from <paramref name="prefab"/>.</returns>
+    public static PrefabTerminalInfo Create(Prefab prefab, Func<ushort, Prefab?> getPrefab)
+    {
+        ImmutableArray<TerminalInfo>.Builder infoBuilder = ImmutableArray.CreateBuilder<TerminalInfo>(2);
+
+        foreach (var (pos, settings) in prefab.Settings)
+        {
+            if ((pos.X | pos.Y | pos.Z) > byte.MaxValue)
+            {
+                continue;
+            }
+
+            // TODO: only use index 0?
+            foreach (var item in settings)
+            {
+                if (item is not { } setting)
+                {
+                    continue;
+                }
+
+                if (setting.Type < SettingType.VoidTerminal)
+                {
+                    continue;
+                }
+
+                var (type, isInput) = SettingTypeUtils.ToTerminalSignalType(setting.Type);
+
+                TerminalDirection dir = prefab.GetTerminalDirection((byte3)pos);
+
+                // isInput always true for terminals of custom prefabs for... reasons???
+                if (prefab.Id >= RawGame.CurrentNumbStockPrefabs)
+                {
+                    isInput = dir is TerminalDirection.PositiveZ or TerminalDirection.NegativeX;
+                }
+                else
+                {
+                    if (type == SignalType.Void && dir is TerminalDirection.PositiveZ or TerminalDirection.NegativeX)
+                    {
+                        isInput = true;
+                    }
+                }
+
+                infoBuilder.Add(new TerminalInfo((byte3)pos, type, dir, isInput)
+                {
+                    Name = setting.Value as string,
+                });
+            }
+        }
+
+        foreach (var connection in prefab.Connections)
+        {
+            if (connection.IsFromOutside && !infoBuilder.Any(terminal => terminal.Position == connection.FromVoxel))
+            {
+                var insidePrefab = getPrefab(prefab.Blocks.GetBlockOrDefault(connection.To));
+
+                if (insidePrefab is not null)
+                {
+                    infoBuilder.Add(new TerminalInfo((byte3)connection.FromVoxel, ResolveBlockTerminalType(insidePrefab, (byte3)connection.ToVoxel, getPrefab), prefab.GetTerminalDirection((byte3)connection.FromVoxel), true));
+                }
+            }
+            else if (connection.IsToOutside && !infoBuilder.Any(terminal => terminal.Position == connection.ToVoxel))
+            {
+                var insidePrefab = getPrefab(prefab.Blocks.GetBlockOrDefault(connection.From));
+
+                if (insidePrefab is not null)
+                {
+                    infoBuilder.Add(new TerminalInfo((byte3)connection.ToVoxel, ResolveBlockTerminalType(insidePrefab, (byte3)connection.FromVoxel, getPrefab), prefab.GetTerminalDirection((byte3)connection.ToVoxel), true));
+                }
+            }
+        }
+
+        return new PrefabTerminalInfo(infoBuilder.DrainToImmutable());
+    }
+
+    /// <summary>
     /// Creates a <see cref="FrozenDictionary{TKey, TValue}"/> of prefab id to <see cref="PrefabTerminalInfo"/> for <see cref="IEnumerable{T}"/> of <see cref="Prefab"/>s.
     /// </summary>
     /// <param name="prefabs">The <see cref="Prefab"/>s to create the <see cref="FrozenDictionary{TKey, TValue}"/> from.</param>
     /// <returns>The <see cref="FrozenDictionary{TKey, TValue}"/> created from <paramref name="prefabs"/>.</returns>
-    public static FrozenDictionary<ushort, PrefabTerminalInfo> Create(IEnumerable<Prefab> prefabs)
+    public static FrozenDictionary<ushort, PrefabTerminalInfo> Create(PrefabList prefabs)
     {
-        Dictionary<ushort, PrefabTerminalInfo> terminalInfos = prefabs.TryGetNonEnumeratedCount(out int count)
-            ? new(count)
-            : new();
+        Dictionary<ushort, PrefabTerminalInfo> terminalInfos = new(prefabs.PrefabCount);
 
         foreach (var prefab in prefabs)
         {
-            terminalInfos.Add(prefab.Id, Create(prefab));
+            terminalInfos.Add(prefab.Id, Create(prefab, prefabs));
         }
 
         return terminalInfos.ToFrozenDictionary();
+    }
+
+    private static SignalType ResolveBlockTerminalType(Prefab prefab, byte3 terminalPos, Func<ushort, Prefab?> getPrefab, int depth = 0)
+    {
+        if (depth > 6)
+        {
+            return SignalType.Error;
+        }
+
+        if (prefab.Settings.TryGetValue(terminalPos, out var settings))
+        {
+            // TODO: only use index 0?
+            foreach (var item in settings)
+            {
+                if (item is not { } setting)
+                {
+                    continue;
+                }
+
+                if (setting.Type < SettingType.VoidTerminal)
+                {
+                    continue;
+                }
+
+                return SettingTypeUtils.ToTerminalSignalType(setting.Type).SignalType;
+            }
+        }
+
+        foreach (var connection in prefab.Connections)
+        {
+            if (connection.IsFromOutside && connection.FromVoxel == terminalPos)
+            {
+                var insidePrefab = getPrefab(prefab.Blocks.GetBlockOrDefault(connection.To));
+
+                if (insidePrefab is not null)
+                {
+                    return ResolveBlockTerminalType(insidePrefab, (byte3)connection.ToVoxel, getPrefab, depth + 1);
+                }
+            }
+            else if (connection.IsToOutside && connection.ToVoxel == terminalPos)
+            {
+                var insidePrefab = getPrefab(prefab.Blocks.GetBlockOrDefault(connection.From));
+
+                if (insidePrefab is not null)
+                {
+                    return ResolveBlockTerminalType(insidePrefab, (byte3)connection.FromVoxel, getPrefab, depth + 1);
+                }
+            }
+        }
+
+        return SignalType.Error;
     }
 }
