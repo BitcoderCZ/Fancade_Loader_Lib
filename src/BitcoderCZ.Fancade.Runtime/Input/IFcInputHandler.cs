de642441f07b595236a4f06283be1bce0f7de02a
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2025-09-11 08:55:25 +0200

    Add FcInputHandler

---
+++ src/BitcoderCZ.Fancade.Runtime/Input/IFcInputHandler.cs
@@ -0,0 +1,29 @@
+﻿using BitcoderCZ.Fancade.Editing.Scripting.Settings;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace BitcoderCZ.Fancade.Runtime.Input;
+
+/// <summary>
+/// Provides a way of getting touches and swipes.
+/// </summary>
+public interface IFcInputHandler
+{
+    /// <summary>
+    /// Gets the position of a touch.
+    /// </summary>
+    /// <param name="state">State of the touch to detect.</param>
+    /// <param name="fingerIndex">The finger whose touch should be detected.</param>
+    /// <param name="touchPos">Position of the touch.</param>
+    /// <returns><see langword="true"/> if the specified touch is active; otherwise, <see langword="false"/>.</returns>
+    bool TryGetTouch(TouchState state, int fingerIndex, out Vector2 touchPos);
+
+    /// <summary>
+    /// Gets the direction of a swipe.
+    /// </summary>
+    /// <param name="direction">Direction of the swipe.</param>
+    /// <returns><see langword="true"/> if a swipe is active; otherwise, <see langword="false"/>.</returns>
+    bool TryGetSwipe(out Vector3 direction);
+}
---
+++ src/BitcoderCZ.Fancade.Runtime/Utils/SpanUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoderCZ.Fancade.Runtime.Utils;
+
+internal static class SpanUtils
+{
+    public static bool AnyTrue(this Span<bool> span)
+        => span.IndexOf(true) != -1;
+    
+    public static bool AnyTrue(this ReadOnlySpan<bool> span)
+        => span.IndexOf(true) != -1;
+}
