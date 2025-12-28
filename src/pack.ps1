ee1a4c1993036e450191a5c3d280689199c36364
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2025-12-28 20:30:28 +0100

    Update pack.ps1

--- src/pack.ps1
+++ src/pack.ps1
@@ -1,4 +1,10 @@
+Param (
+	[Parameter(Mandatory=$false)]
+	[ValidateSet("Debug", "Release")]
+	[string]$Configuration = 'Release'
+)
+
 Remove-Item "./nupkgs/*"
 dotnet clean
-dotnet build -c Release
-dotnet pack -c Release -o nupkgs/
+dotnet build -c $Configuration -p:WarningLevel=0
+dotnet pack -c $Configuration -o nupkgs/ -p:WarningLevel=0
